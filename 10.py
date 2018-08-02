ec78c70ba27d23d20a7f2ce4ccab823becbdf020
Kenzie Bottoms <kenziebottoms@gmail.com>
2018-08-02 10:11:03 -0500

    trimmed down time for #10 by looking up shortcuts

--- 10.py
+++ 10.py
@@ -2,23 +2,20 @@
 #   find the sum of all the prime numbers under 2 000 000
 #     answer = 142913828922
 
-#def is_prime(x):
-#  for i in range (2,x/2+1):
-#    if x % i == 0:
-#      return False
-#  return True
-
-primes = []
+import math
+primes = [2]
 
 def is_prime(x):
   for prime in primes:
     if x % prime == 0:
       return False
+    if math.sqrt(x) < prime:
+      break
   primes.append(x)
   return True
 
 for i in range(3,2000000,2):
-  if is_prime(i) & (str(i)[-2:] == "17"):
+  if is_prime(i) and (str(i)[-3:] == "317"):
     print i
 
-print sum(primes+2)
+print sum(primes)
