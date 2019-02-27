e504fd982a352609837cc3e3b46233faa4fe795a
Kenzie Bottoms <kenziebottoms@gmail.com>
2019-02-27 12:51:14 -0600

    solve 23 but bad time

--- 23.py
+++ 23.py
@@ -1,23 +1,18 @@
 # project euler no. 23
 #   find the sum of all positive integers that cannot be written as the sum of two abundant numbers
 
-
-#ID FUNCTIONS
+from sets import Set
 
 def is_abundant(x):
-  if sum(get_divisors(x)) > x:
-    return True
-  return False
-
-def is_sum_of_abundants(x,abundants):
-  for item in abundants:
-    for item2 in abundants:
-      if item+item2 == x:
+  return sum(get_divisors(x)) > x
+    
+def is_sum_of_abundants(n,abundants):
+  for x in [x for x in abundants if x < n]:
+    for y in [y for y in abundants if y < n]:
+      if x+y == n:
+        print x,"+",y,"=",n
         return True
   return False
-
-
-#GET FUNCTIONS
 
 def get_divisors(x):
   divisors = [1]
@@ -26,40 +21,7 @@ def get_divisors(x):
       divisors.append(i)
   return divisors
 
-#print get_divisors(12)
-
-def get_abundants():
-  abundants = []
-  for i in range(12,28124):
-    if is_abundant(i):
-      print i
-      abundants.append(i)
-  return abundants
-
-get_abundants()
-
-def get_sums(abundants):
-  sums = []
-  for i in range(0,28124):
-    if is_sum_of_abundants(i,abundants):
-      sums.append(i)
-  return sums
-
-def get_nonsums(sums):
-  nonsums = []
-  for i in range(0,28124):
-    if not i in sums:
-      nonsums.append(i)
-  return nonsums
-
-
-#EXECUTION
-
-#abundants = get_abundants()
-#print abundants[0:100]
-#print is_sum_of_abundants(24,abundants)
-#print is_sum_of_abundants(27,abundants)
-#print is_sum_of_abundants(30,abundants)
-#sums = get_sums(abundants)
-#nonsums = get_nonsums(sums)
-#print sum(nonsums)
+abundants = Set(filter(lambda x:is_abundant(x), Set(range(12,28124))))
+sums = Set(filter(lambda x:is_sum_of_abundants(x, abundants), Set(range(1,28124))))
+nonsums = Set(range(1,28124)) - sums
+print sum(nonsums)
--- README.md
+++ README.md
@@ -11,7 +11,7 @@ python 01.py    # unless otherwise specified
 | :heavy_check_mark: [6](06.py) | :heavy_check_mark: [7](07.py) | :heavy_check_mark: [8](08.py) | :heavy_check_mark: [9](09.py) | :heavy_check_mark: [10](10.py) |
 | :heavy_check_mark: [11](11.py) | :heavy_check_mark: [12](12.py) | :heavy_check_mark: [13](13.py) | :heavy_check_mark: [14](14.py) | :heavy_check_mark: [15](15.py) |
 | :heavy_check_mark: [16](16.py) | :heavy_check_mark: [17](17.py) | :heavy_check_mark: [18](18.py) | :heavy_check_mark: [19](19.py) | :heavy_check_mark: [20](20.py) |
-| :heavy_check_mark: :watch: [21](21.py) | :heavy_check_mark: [22](22.py) | :x: [23](23.py) | 24 | :heavy_check_mark: [25](25.py) |
+| :heavy_check_mark: :watch: [21](21.py) | :heavy_check_mark: [22](22.py) | :heavy_check_mark: :watch: [23](23.py) | 24 | :heavy_check_mark: [25](25.py) |
 | 26 | 27 | 28 | 29 | 30 |
 | 31 | 32 | 33 | 34 | 35 |
 | 36 | 37 | 38 | 39 | 40 |
